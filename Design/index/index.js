e8a783e76604c8adc21daa457451591204781fd1
GabrielRosaNunes <30870653+GabrielRosaNunes@users.noreply.github.com>
2019-06-09 17:37:28 -0300

    Add files via upload

--- Design/index/index.js
+++ Design/index/index.js
@@ -1,44 +1,40 @@
 var controlMenu = 0;
 $(document).ready(function(){
-    
+
     $("#toggler").click(function(){
         if (controlMenu == 0 ) {
-            $("#menu, #menu div,#menuUl,#menuUl li").animate({
+            $("#menu, #menu div#menudiv,#menuUl,#menuUl li").animate({
                 width: "0px"
             },{
                 duration: 500,
                 progress: function(animation, progress, msRemaining) {
                     largura = $("#menu").width();
                     $("#conteudo").attr("style","float:left;padding-top:76px;padding-left:"+largura+"px;width:100%;overflow-x: auto");
-                    if(progress > 0.65){
+                    if(progress > 0.60){
                         $("#menuUl li").attr('style','display:none')
                     }
                 }
             });
             controlMenu = 1;
         } else {
-            
-            $("#menu, #menu div,#menuUl,#menuUl li").animate({
+
+            $("#menu, #menu div#menudiv,#menuUl,#menuUl li").animate({
                 width: "300px"
             },{
                 duration: 500,
                 progress: function(animation, progress, msRemaining) {
                     largura = $("#menu").width();
                     $("#conteudo").attr("style","float:left;padding-top:76px;padding-left:"+largura+"px;width:100%;overflow-x: auto");
-                    if(progress > 0.65){
+                    if(progress > 0.60){
                         $("#menuUl li").attr('style','display:block')
                     }
                 },
                 start: function () {
-                    
+
                 }
             });
             controlMenu = 0;
         }
-        
+
     })
 })
-
-$(document).ready(function() {
-    $('#example').DataTable();
-} );
